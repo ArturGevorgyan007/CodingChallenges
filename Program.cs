6a8bd25253b4576e1e17182b3566130f52af6dd7
Artur <arturgevorgyan87@yahoo.com>
2023-04-17 19:44:59 -0700

    Coding Challenge 04172023

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+public class CodeChalange041723{
+    static int[] IndexOfCapitals (string s) {
+        int[] res = new int[NumOfCapitals(s)];
+        int i =0;
+        foreach (char ch in s) {
+            if(char.IsAsciiLetterUpper(ch)) {
+                res[i]=s.IndexOf(ch);
+                i++;
+            }
+        }
+        return res;
+    }
+
+    static int NumOfCapitals(string s) {
+        int i=0;
+        foreach (char ch in s) {
+            if (char.IsAsciiLetterUpper(ch))
+                i++;
+        }
+        return i;
+    }
+    
+
+    public static void Main() {
+        System.Console.WriteLine("Enter single string");
+        string s = System.Console.ReadLine();
+        System.Console.WriteLine("Result is: ");
+        System.Console.WriteLine("[{0}]", string.Join(", ", IndexOfCapitals(s)));
+    }
+}
+
