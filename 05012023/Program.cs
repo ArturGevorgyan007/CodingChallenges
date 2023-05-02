4febc44834f916202ef690862e1c181406f351e2
Artur <arturgevorgyan87@yahoo.com>
2023-05-01 20:34:05 -0700

    New Challenge solved 01052023

---
+++ 05012023/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = Int32.Parse(Console.ReadLine());
+
+int func(int n) {
+    int step = 0;
+    while(n!=1) {
+        if(n%2==0) 
+            n=n/2;
+        else
+            n=3*n+1;
+        step++;      
+    }
+    return step;
+}
+
+Console.WriteLine(func(n));
