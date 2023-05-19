732b47836e577a0fcf6067cbaa75fcacbd490fa2
Artur <arturgevorgyan87@yahoo.com>
2023-05-19 16:54:41 -0700

    New challenge based on Raj code

---
+++ 05192023/Program.cs
@@ -0,0 +1,78 @@
+﻿﻿//C# program for:
+//Given two (singly) linked lists, determine if the two lists intersect. Return the intersecting node. 
+//Note that the intersection is defined based on reference, not value. 
+//That is, if the kth node of the first linked list is the exact same node (by reference) as the jth node of the second linked list, then they are intersecting.
+//This is Raj's code, I changed it, as it did not work when Linked Lists' lengths are equal
+
+using System;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode ptrA = headA;
+        ListNode ptrB = headB;
+
+        while (ptrA != ptrB) {
+
+            while (ptrB!=null)
+            {
+                ptrB = ptrB.next;
+
+                if (ptrA == ptrB)
+                    return ptrA;
+            }
+
+            ptrA = ptrA.next;
+            ptrB=headB;
+
+        }
+
+        return ptrA;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create the first linked list: 1->2->3->4->5
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(3);
+        // list1.next.next.next = new ListNode(4);
+        // list1.next.next.next.next = new ListNode(5);
+
+        // Create the second linked list: 6->7->8
+        ListNode list2 = new ListNode(6);
+        list2.next = new ListNode(7);
+        list2.next.next = new ListNode(8);
+
+        // Connect the two lists at node 4
+        list2.next = list1.next;
+
+        // Find the intersection node
+        Solution solution = new Solution();
+        ListNode intersectionNode = solution.GetIntersectionNode(list1, list2);
+
+        if (intersectionNode != null)
+            Console.WriteLine("Intersection Node Value: " + intersectionNode.val);
+        else
+            Console.WriteLine("No Intersection Node");
+    }
+}
